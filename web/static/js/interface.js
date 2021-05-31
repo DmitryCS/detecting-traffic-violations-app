bf54cb0ac48ea29d9ed2e7f3c1776c2bdc89c0d9
Dmitry <dmitry.lyzhin2014@yandex.ru>
2021-06-01 01:13:30 +0500

    clearing v2

--- web/static/js/interface.js
+++ web/static/js/interface.js
@@ -1,8 +1,4 @@
-globalThis.videos = {'kompol-11s':"./static/raw/blanks/cut_final_10sek_v2_final_with_crossing.mp4"}
-// $( "#road-region-select" ).change(function() {
-//     if (this.value == "custom") $("#custom-road-region-files").show();
-//     else $("#custom-road-region-files").hide();
-//     });
+globalThis.videos = {'kompol-9s':"./static/raw/blanks/0512_ready.mp4"}
 $( "#video-select" ).change(function() {
     $("#video-view").attr("src","");
     setVideoFileUrl();
@@ -18,7 +14,7 @@ $( "#video-select" ).change(function() {
         $("#video-form2").hide();
         $("#youtube-link").hide();
     }
-    if (this.value == "kompol-11s") {
+    if (this.value == "kompol-9s") {
         setVideoFileUrl(globalThis.videos[this.value], "video/mp4");
     }
 });
@@ -36,7 +32,6 @@ $("#but-download").click(function (){
     document.getElementById("youtube-link").src = "https://www.youtube.com/embed/"+videoId;
     $("#youtube-link").show();
 });
-//$("#mask-file").change(function(){showUploadImage(this, "#mask-view")});
 
 let STATE = {UPLOAD: 1, PROGRESS: 2, DOWNLOAD: 3};
 
@@ -69,7 +64,6 @@ class InterfaceState {
 }
 
 let interfaceState = new InterfaceState(STATE.UPLOAD);
-// document.getElementById("to-upload-state").onclick = () => interfaceState.setState(STATE.PROGRESS);
 
 
 function setVideoFileUrl(url, type, id_video_view = "#video-view") {
@@ -91,30 +85,10 @@ function setVideoFileUrl(url, type, id_video_view = "#video-view") {
     }
     else $("#video-view").hide();
 }
-//
-// function showUploadImage(form, showImageElementSelector)
-// {
-//
-//     let file = form.files[0];
-//     console.log(file.name);
-//     if (file)
-//     {
-//         $(showImageElementSelector).show();
-//         $(showImageElementSelector).attr("src", URL.createObjectURL(file));
-//     }
-//     else $(showImageElementSelector).hide();
-//
-// }
-//
+
 $("#video-file").change(setVideoFileUrl);
 setVideoFileUrl(document.getElementById("youtube-video").value, "video/mp4")
-//
-//
-// $("#video-file").change();
-// $("#video-select" ).change();
-// $("#road-region-select" ).change();
-//
-//
+
 $("#send-video").click(function (){
     let file = document.getElementById("video-file").files[0];
     let youtube_file = document.getElementById("youtube-video").value;
@@ -129,7 +103,6 @@ $("#send-video").click(function (){
             processData: false,
             contentType: false,
             cache: false,
-            // timeout: 600000,
             success: function (response) {
                 console.log(response);
                 if(response['existing']){
@@ -165,80 +138,7 @@ $("#send-video").click(function (){
         });
     }
 })
-// $("#sendVideoForm").submit(function(e){
-//     e.preventDefault();
-//
-//     let body = getFormData();
-//     if (!body) return;
-//
-//     $.ajax({
-//         url: globalThis.serverLink,
-//         type: "POST",
-//         data: body,
-//         processData: false,
-//         contentType: false,
-//         success: function(data){
-//             console.log(data);
-//             console.log('success');
-//             setTimeout(() => checkProcess(data.id),2000);
-//         },
-//         error: function (error) {
-//             alert('Server not run or error on server. Write to alx.grents@gmail.com');
-//         }
-//     });
-//     console.log(Object.fromEntries(body))
-//     console.log('send');
-//     return false;
-// });
-//
-// function getFormData(){
-//     let body = new FormData();
-//
-//     let video = $("#video-select").val();
-//     if (video == "custom") {
-//         body.set("video", "custom");
-//         let file = document.getElementById('video-file').files[0];
-//         if (file) body.set("video-file", file, file.name);
-//         else {
-//             alert("Укажите видео-файл");
-//             return false;
-//         }
-//     }
-//     else body.set("video", video);
-//
-//     let roadRegion = $("#road-region-select").val();
-//     if (roadRegion == "custom")
-//     {
-//         body.set("roadRegion", "custom");
-//
-//         let coefsFile = document.getElementById('coefs-file').files[0];
-//         let maskFile = document.getElementById('mask-file').files[0];
-//
-//         if (coefsFile && maskFile) {
-//             body.set("coefsFile", coefsFile, coefsFile.name);
-//             body.set("maskFile", maskFile, maskFile.name);
-//         }
-//         else {
-//             alert("Укажите файлы с маской и коэффициентами")
-//             return false;
-//         }
-//     }
-//     else body.set("roadRegion", roadRegion);
-//
-//
-//     let outputFormat = $('input[name="outputParameters"]:checked').val();
-//     body.set("outputFormat", outputFormat);
-//
-//     body.set("bbox", $("#bbox").prop("checked"))
-//     body.set("class", $("#class").prop("checked"))
-//     body.set("position", $("#position").prop("checked"))
-//     body.set("speed", $("#speed").prop("checked"))
-//
-//     return body;
-// }
-//
-// globalThis.serverLink = location.href;
-//
+
 async function checkProgress(processId, file_name)
 {
     console.log('check on', processId)
@@ -249,7 +149,6 @@ async function checkProgress(processId, file_name)
             updateProgressbar(data);
             if (data['progress_percantage'] == 100){
                 console.log('FILE IS READY');
-                // interfaceState.setState(STATE.DOWNLOAD);
                 clearInterval(interfaceState.progressUpdateTimerId);
                 setVideoFileUrl("/static/raw/"+file_name,"video/mp4", "#video-download");
                 interfaceState.setState(STATE.DOWNLOAD);
@@ -257,30 +156,9 @@ async function checkProgress(processId, file_name)
                 $("#download-mp4-file").attr('href', "/static/raw/"+file_name);
             }
             else{
-                //document.getElementById("detectionProgressbar").innerHTML = data['progress_percantage'].toString() + "%";
                 console.log(data['progress_percantage'])
             }
-            // document.getElementById("detectionProgressbar").innerHTML = "dasdasd";
-            // if (data.status == 'run')
-            // {
-            //     updateProgressbar(data)
-            //     interfaceState.setState(STATE.PROGRESS, processId)
-            //     if (interfaceState.progressUpdateTimerId === null)
-            //     {
-            //         interfaceState.progressUpdateTimerId = setInterval(checkProcess, 5000, processId)
-            //     }
-            //
-            // }
-            // else if (data.status == 'end')
-            // {
-            //     interfaceState.setState(STATE.DOWNLOAD);
-            //
-            //     setDownloadLinks(data)
-            // }
-            // else if (data.status == 'start')
-            // {
-            //     interfaceState.setState(STATE.PROGRESS)
-            // }
+
         },
         error: function (error) {
             interfaceState.setState(STATE.UPLOAD)
@@ -291,86 +169,4 @@ async function checkProgress(processId, file_name)
 function updateProgressbar(data){
     console.log(data)
     $("#detectionProgressbar").text(data["progress_percantage"].toString() + "%");
-    // $("#detectionProgressbar").attr("aria-valuenow", data.currentFrame);
-    // $("#detectionProgressbar").attr("aria-valuemax", data.frameCount);
-    // $("#detectionProgressbar").css("width", 100* data.currentFrame/data.frameCount + '%');
-    // $("#detectionProgressbar").text(Math.round(100* data.currentFrame/data.frameCount, 2) + '%');
 }
-//
-// function setDownloadLinks(data) {
-//     console.log(data)
-//     if (data.mp4 && data.webm){
-//         let mp4Link = globalThis.files + '/' + data.mp4;
-//         let webmLink = globalThis.files + '/' + data.webm;
-//         $("#video-download>video").attr('src', webmLink);
-//
-//         $("#download-mp4-file").attr('href', mp4Link);
-//         $("#download-webm-file").attr('href', webmLink);
-//
-//         $("#video-download>video").hidden = false;
-//         $("#download-mp4-file").hidden = false;
-//         $("#download-webm-file").hidden = false;
-//
-//     }
-//     else {
-//         $("#video-download>video").hidden = true;
-//         $("#download-video-file").hidden = true;
-//     }
-//
-//     if (data.json)
-//     {
-//         let jsonLink = globalThis.files + '/' + data.json;
-//         $("#download-json-file").attr('href', jsonLink);
-//         $("#download-json-file").hidden = false;
-//     }
-//     else {
-//         $("#download-json-file").hidden = true;
-//     }
-//
-//     setResultLink(data.id)
-//
-// }
-//
-// function setResultLink(processId)
-// {
-//     let processLink = new URL(location.href);
-//     processLink.searchParams.set('processId', processId)
-//
-//     $(".url-for-result>a").text(processLink);
-//     $(".url-for-result>a").attr('href', processLink)
-// }
-//
-// function setUrls(url, params)
-// {
-//     globalThis.serverHost = url;
-//     globalThis.serverLink = globalThis.serverHost + params.detect;
-//     globalThis.progressLink = globalThis.serverHost + params.result;
-//     globalThis.files = globalThis.serverHost + '/file'
-// }
-//
-//
-//
-// async function setUrlsFromProxy()
-// {
-//     $.ajax({
-//         url: "https://speed-detection-proxy.herokuapp.com/server",
-//         success: function (data)
-//         {
-//             setUrls(data.host, data)
-//         }
-//     })
-// }
-//
-// async function start(){
-//     let url = new URL(location.href);
-//     let processId = url.searchParams.get('processId');
-//     setUrlsFromProxy();
-//     if (processId)
-//     {
-//         console.log('processId', processId)
-//         setTimeout(() => checkProcess(processId), 1000);
-//     }
-//
-//
-// }
-// document.addEventListener("DOMContentLoaded", start);
